f21e9daec8f36a4c4b717f79d5751a71b4357e99
paul <paulbrowne2@gmail.com>
2025-02-05 19:34:01 +0000

    Added docker-compose

--- internal/db/connect.go
+++ internal/db/connect.go
@@ -7,25 +7,15 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 func ConnectDB() (*sql.DB, error) {
-	if err := godotenv.Load(); err != nil {
-		log.Println("Warning: No .env file found, using system environment variables.")
-	}
-
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
-	// dbHost := os.Getenv("DB_HOST")
 	dbHost := os.Getenv("DB_HOST")
-	if dbHost == "localhost" || dbHost == "" {
-		dbHost = "127.0.0.1" // Force IPv4 instead of IPv6 (::1)
-	}
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	db, err := sql.Open("mysql", dsn)
@@ -34,7 +24,7 @@ func ConnectDB() (*sql.DB, error) {
 	}
 
 	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("error connecting to database: %w", err)
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
 
 	log.Println("Connected to MySQL successfully.")
